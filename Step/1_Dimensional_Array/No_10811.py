d6ca19fdee9650c041f59733b8a282ffa2e538f8
Lee Jaewon <leejaewon264@gmail.com>
2025-05-12 13:35:13 +0900

    No_10811:바구니 뒤집기  1.슬라이싱 basket[i-1:j]  2.리스트 뒤집기[::-1]

---
+++ Step/1_Dimensional_Array/No_10811.py
@@ -0,0 +1,15 @@
+# 바구니 뒤집기
+
+# N, M 입력 및 리스트 초기화
+N, M = map(int, input().split())
+basket = list(range(1, N+1))
+
+# i, j 입력 받아서 슬라이싱 및 리스트 뒤집기
+for _ in range(M):
+    i, j  = map(int, input().split())
+
+    # [start:end:-1] start와 stop을 생략하면 전체 뒤에서 앞으로를 의미
+    # basket[i-1:j][::-1] -> i-1에서 j까지 슬라이싱 후 뒤집음 
+    basket[i-1:j] = basket[i-1:j][::-1]
+
+print(*basket)
